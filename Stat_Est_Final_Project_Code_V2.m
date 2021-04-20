2dc1e757c6c05e2ce74d12ded09e041a73dc8e91
elliottmckee <elliott.mckee@colorado.edu>
2021-04-19 19:00:07 -0600

    No functional changes, just comments + readability changes

--- Stat_Est_Final_Project_Code_V2.m
+++ Stat_Est_Final_Project_Code_V2.m
@@ -1,24 +1,47 @@
-% Stat Est Project
+%% ASEN 5033 State Estimation Project- Coop Localization
+%{
+Team Members:
+-Elliott McKee
+-Robert S.
+-Vishal
 
-% Part 1
-% Problem 1
+%}
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Part 1 %%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%  Problem 1
+%Geometric Constants
 L = 0.5;
 % va = 12;
 % wa = pi()/25;
+
+%Symbolic Math Variables
 syms xg eg tg xa ea ta vg pg va wa delta_t y v x f
 
-x = [xg eg tg xa ea ta];
-f = [vg*cos(tg),vg*sin(tg),vg/L*tan(pg), va*cos(ta),va*sin(ta), wa];
-A = jacobian(f,x);
-% Problem 2
+% State Vector
+x = [xg;      %[m] AGV East Pos (Xi)
+       eg;      %[m] AGV East Pos (eta)
+       tg;       %[RAD] AGV Orientation
+       xa;       %[m] UAV East position (Xi)
+       ea;       %[m] UAV East Pos (eta)
+       ta];       %[RAD] UAV 
+
+% 
+f = [vg*cos(tg),    vg*sin(tg),     vg/L*tan(pg),        va*cos(ta),va*sin(ta),      wa];
+
+A_sym = jacobian(f,x);
 
 
+
+%%  Problem 2
 % At initial point in the nominal trajectory
 a= subs(A,[vg, va, tg, ta], [2, 12, pi/2, -pi/2]);
 
-
-dt = 0.1; % discrete time
+%Discrete time timestep
+dt = 0.1; 
 
 % Variable F matrix
 F = expm(A.*delta_t); 
@@ -44,14 +67,14 @@ for idx = 1:6
     O((idx-1)*5+1:idx*5,:) = HH*FF^(idx-1);
 end
 
+% If we need to do this at each state we can just do for loop. save ranks
+% and stabilities
+
 
 % Obs Rank = 6 n = 6 full column rank, no observability issues
 Obs = rank(O);
 
 % Find the Eigen Values of F
 [V, D] = eig(F);
-
-% If we need to do this at each state we can just do for loop. save ranks
-% and stabilities
 
 

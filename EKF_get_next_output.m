de233dd521928038afa0f4c9d7494b6329f0f56e
Vish <vish9749@colorado.edu>
2021-05-01 11:13:00 -0600

    Some modules for EKF

---
+++ EKF_get_next_output.m
@@ -0,0 +1,19 @@
+function [y_k_plus_1_minus] = EKF_get_next_output(x_k_plus_1_minus)
+
+[eta_g,nu_g,theta_g,eta_a,nu_a,theta_a]=EKF_breakout_state_vector(x_k_plus_1_minus);
+
+y1=atan2((nu_a-nu_g),(eta_a-eta_g))-theta_g;
+
+y2=sqrt((eta_g-eta_a)^2+(nu_g-nu_a)^2);
+
+y3=atan2((nu_g-nu_a),(eta_g-eta_a))-theta_a;
+
+y4=eta_a;
+
+y5=nu_a;
+
+y_k_plus_1_minus=[y1;y2;y3;y4;y5];
+
+
+end
+
---
+++ EKF_get_next_state.m
@@ -0,0 +1,4 @@
+function [c_k_plus_1_minus] = EKF_get_next_state(x_k_plus)
+
+end
+
